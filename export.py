05ace0aa0d4abb620881c295e062796bf8327d08
pinto0309 <rmsdh122@yahoo.co.jp>
2024-01-24 23:25:13 +0900

    tensorrt

--- export.py
+++ export.py
@@ -31,14 +31,14 @@ def parse_args() -> argparse.Namespace:
 def export_onnx(model: str, output: str = None):
     # Handle args
     if output is None:
-        output = f"weights/depth_anything_vit{model}14.onnx"
+        output = f"depth_anything_vit{model}14.onnx"
 
     # Device for tracing (use whichever has enough free memory)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = "cpu"
 
     # Sample image for tracing (dimensions don't matter)
-    image, _ = load_image("assets/sacre_coeur1.jpg")
-    image = torch.from_numpy(image).to(device)
+    # image, _ = load_image("assets/sacre_coeur1.jpg")
+    # image = torch.from_numpy(image).to(device)
 
     # Load model params
     if model == "s":
@@ -61,25 +61,158 @@ def export_onnx(model: str, output: str = None):
         ),
         strict=True,
     )
+    depth_anything.cpu()
     depth_anything.eval()
 
-    torch.onnx.export(
-        depth_anything,
-        image,
-        output,
-        input_names=["image"],
-        output_names=["depth"],
-        opset_version=17,
-        dynamic_axes={
-            "image": {2: "height", 3: "width"},
-            "depth": {2: "height", 3: "width"},
-        },
-    )
 
-    save_model(
-        SymbolicShapeInference.infer_shapes(load_model(output), auto_merge=True),
-        output,
-    )
+
+    import onnx
+    from onnxsim import simplify
+    RESOLUTION = [
+            # [192,320],
+        # [192,416],
+        # [192,640],
+        # [192,800],
+            # [256,320],
+        # [256,416],
+        # [256,448],
+        # [256,640],
+        # [256,800],
+        # [256,960],
+        # [288,480],
+        # [288,640],
+        # [288,800],
+        # [288,960],
+        # [288,1280],
+        # [320,320],
+        # [384,480],
+            # [384,640],
+        # [384,800],
+        # [384,960],
+        # [384,1280],
+        # [416,416],
+        [490,644],
+        # [480,800],
+        # [480,960],
+        # [480,1280],
+        # [512,512],
+        # [512,640],
+            # [512,896],
+        # [544,800],
+        # [544,960],
+        # [544,1280],
+        # [640,640],
+            # [736,1280],
+    ]
+    MODEL = f'depth_anything_vit{model}14'
+    for H, W in RESOLUTION:
+        onnx_file = f"{MODEL}_1x3x{H}x{W}.onnx"
+        x = torch.randn(1, 3, H, W).cpu()
+        torch.onnx.export(
+            depth_anything,
+            args=(x),
+            f=onnx_file,
+            opset_version=11,
+            input_names=['input'],
+            output_names=['output'],
+        )
+        model_onnx1 = onnx.load(onnx_file)
+        model_onnx1 = onnx.shape_inference.infer_shapes(model_onnx1)
+        onnx.save(model_onnx1, onnx_file)
+        model_onnx2 = onnx.load(onnx_file)
+        model_simp, check = simplify(model_onnx2)
+        onnx.save(model_simp, onnx_file)
+        model_onnx2 = onnx.load(onnx_file)
+        model_simp, check = simplify(model_onnx2)
+        onnx.save(model_simp, onnx_file)
+        model_onnx2 = onnx.load(onnx_file)
+        model_simp, check = simplify(model_onnx2)
+        onnx.save(model_simp, onnx_file)
+
+        onnx_file = f"{MODEL}_Nx3x{H}x{W}.onnx"
+        x = torch.randn(1, 3, H, W).cpu()
+        torch.onnx.export(
+            depth_anything,
+            args=(x),
+            f=onnx_file,
+            opset_version=11,
+            input_names=['input'],
+            output_names=['output'],
+            dynamic_axes={
+                'input' : {0: 'N'},
+                'output' : {0: 'N', 1: '1', 2: f'{H}', 3: f'{W}'},
+            }
+        )
+        model_onnx1 = onnx.load(onnx_file)
+        model_onnx1 = onnx.shape_inference.infer_shapes(model_onnx1)
+        onnx.save(model_onnx1, onnx_file)
+        model_onnx2 = onnx.load(onnx_file)
+        model_simp, check = simplify(model_onnx2)
+        onnx.save(model_simp, onnx_file)
+        model_onnx2 = onnx.load(onnx_file)
+        model_simp, check = simplify(model_onnx2)
+        onnx.save(model_simp, onnx_file)
+        model_onnx2 = onnx.load(onnx_file)
+        model_simp, check = simplify(model_onnx2)
+        onnx.save(model_simp, onnx_file)
+
+
+
+    # onnx_file = f"{MODEL}_1x3xHxW.onnx"
+    # x = torch.randn(1, 3, 192, 320).cpu()
+    # torch.onnx.export(
+    #     depth_anything,
+    #     args=(x),
+    #     f=onnx_file,
+    #     opset_version=11,
+    #     input_names=['input'],
+    #     output_names=['output'],
+    #     dynamic_axes={
+    #         'input' : {2: 'height', 3: 'width'},
+    #         'output' : {2: 'height', 3: 'width'},
+    #     }
+    # )
+    # save_model(
+    #     SymbolicShapeInference.infer_shapes(load_model(onnx_file), auto_merge=True),
+    #     onnx_file,
+    # )
+    # model_onnx2 = onnx.load(onnx_file)
+    # model_simp, check = simplify(model_onnx2)
+    # onnx.save(model_simp, onnx_file)
+    # model_onnx2 = onnx.load(onnx_file)
+    # model_simp, check = simplify(model_onnx2)
+    # onnx.save(model_simp, onnx_file)
+    # model_onnx2 = onnx.load(onnx_file)
+    # model_simp, check = simplify(model_onnx2)
+    # onnx.save(model_simp, onnx_file)
+
+    # onnx_file = f"{MODEL}_Nx3xHxW.onnx"
+    # x = torch.randn(1, 3, 192, 320).cpu()
+    # torch.onnx.export(
+    #     depth_anything,
+    #     args=(x),
+    #     f=onnx_file,
+    #     opset_version=11,
+    #     input_names=['input'],
+    #     output_names=['output'],
+    #     dynamic_axes={
+    #         'input' : {0: 'N', 2: 'height', 3: 'width'},
+    #         'output' : {0: 'N', 2: 'height', 3: 'width'},
+    #     }
+    # )
+    # save_model(
+    #     SymbolicShapeInference.infer_shapes(load_model(onnx_file), auto_merge=True),
+    #     onnx_file,
+    # )
+    # model_onnx2 = onnx.load(onnx_file)
+    # model_simp, check = simplify(model_onnx2)
+    # onnx.save(model_simp, onnx_file)
+    # model_onnx2 = onnx.load(onnx_file)
+    # model_simp, check = simplify(model_onnx2)
+    # onnx.save(model_simp, onnx_file)
+    # model_onnx2 = onnx.load(onnx_file)
+    # model_simp, check = simplify(model_onnx2)
+    # onnx.save(model_simp, onnx_file)
 
 
 if __name__ == "__main__":
--- infer.py
+++ infer.py
@@ -10,10 +10,10 @@ from depth_anything.util.transform import load_image
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--img",
-        type=str,
-        required=True,
-        help="Path to input image.",
+        "--device",
+        type=int,
+        default=0,
+        help="device number",
     )
     parser.add_argument(
         "--model",
@@ -21,65 +21,95 @@ def parse_args() -> argparse.Namespace:
         required=True,
         help="Path to ONNX model.",
     )
-    parser.add_argument(
-        "--viz", action="store_true", help="Whether to visualize the results."
-    )
     return parser.parse_args()
 
 
-def infer(img: str, model: str, viz: bool = False):
-    image, (orig_h, orig_w) = load_image(img)
-
+def infer(device: int, model: str, viz: bool = True):
     session = ort.InferenceSession(
-        model, providers=["CUDAExecutionProvider", "CPUExecutionProvider"]
+        model, providers=[
+            (
+                'TensorrtExecutionProvider', {
+                    'trt_engine_cache_enable': True,
+                    'trt_engine_cache_path': '.',
+                    'trt_fp16_enable': True,
+                }
+            ),
+            "CUDAExecutionProvider",
+            "CPUExecutionProvider"
+        ]
     )
-    depth = session.run(None, {"image": image})[0]
+    i_n, i_c, i_h, i_w = session.get_inputs()[0].shape
+    cap = cv2.VideoCapture(device)
+    cap_fps = cap.get(cv2.CAP_PROP_FPS)
+    w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+    video_writer = cv2.VideoWriter(
+        filename='output.mp4',
+        fourcc=fourcc,
+        fps=cap_fps,
+        frameSize=(w, h*2),
+    )
 
-    depth = cv2.resize(depth[0, 0], (orig_w, orig_h))
-    depth = (depth - depth.min()) / (depth.max() - depth.min()) * 255.0
-    depth = depth.astype(np.uint8)
-    depth_color = cv2.applyColorMap(depth, cv2.COLORMAP_INFERNO)
+    while True:
+        orig_image, inference_image, (orig_h, orig_w) = load_image(cap, i_h, i_w)
+        if orig_image is None:
+            break
 
-    # Visualization
-    if viz:
-        margin_width = 50
-        caption_height = 60
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        font_scale = 1
-        font_thickness = 2
-        split_region = np.ones((orig_h, margin_width, 3), dtype=np.uint8) * 255
-        combined_results = cv2.hconcat([cv2.imread(img), split_region, depth_color])
+        depth = session.run(None, {"input": inference_image})[0]
 
-        caption_space = (
-            np.ones((caption_height, combined_results.shape[1], 3), dtype=np.uint8)
-            * 255
-        )
-        captions = ["Raw image", "Depth Anything"]
-        segment_width = orig_w + margin_width
-        for i, caption in enumerate(captions):
-            # Calculate text size
-            text_size = cv2.getTextSize(caption, font, font_scale, font_thickness)[0]
+        depth = cv2.resize(depth[0, 0], (orig_w, orig_h))
+        depth = (depth - depth.min()) / (depth.max() - depth.min()) * 255.0
+        depth = depth.astype(np.uint8)
+        depth_color = cv2.applyColorMap(depth, cv2.COLORMAP_INFERNO)
 
-            # Calculate x-coordinate to center the text
-            text_x = int((segment_width * i) + (orig_w - text_size[0]) / 2)
+        # Visualization
+        if viz:
+            margin_width = 50
+            caption_height = 60
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            font_scale = 1
+            font_thickness = 2
+            split_region = np.ones((orig_h, margin_width, 3), dtype=np.uint8) * 255
+            combined_results = cv2.hconcat([orig_image, split_region, depth_color])
 
-            # Add text caption
-            cv2.putText(
-                caption_space,
-                caption,
-                (text_x, 40),
-                font,
-                font_scale,
-                (0, 0, 0),
-                font_thickness,
+            caption_space = (
+                np.ones((caption_height, combined_results.shape[1], 3), dtype=np.uint8)
+                * 255
             )
+            captions = ["Raw image", "Depth Anything"]
+            segment_width = orig_w + margin_width
+            for i, caption in enumerate(captions):
+                # Calculate text size
+                text_size = cv2.getTextSize(caption, font, font_scale, font_thickness)[0]
 
-        final_result = cv2.vconcat([caption_space, combined_results])
+                # Calculate x-coordinate to center the text
+                text_x = int((segment_width * i) + (orig_w - text_size[0]) / 2)
 
-        cv2.imshow("depth", final_result)
-        cv2.waitKey(0)
+                # Add text caption
+                cv2.putText(
+                    caption_space,
+                    caption,
+                    (text_x, 40),
+                    font,
+                    font_scale,
+                    (0, 0, 0),
+                    font_thickness,
+                )
 
-    return depth
+            final_result = cv2.vconcat([caption_space, combined_results])
+
+            cv2.imshow("depth", final_result)
+            video_writer.write(np.vstack([orig_image, depth_color]))
+            key = cv2.waitKey(1)
+            if key == 27: # ESC
+                break
+
+    if video_writer:
+        video_writer.release()
+
+    if cap:
+        cap.release()
 
 
 if __name__ == "__main__":
